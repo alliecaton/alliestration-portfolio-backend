a2e10879ad98c3f608a487600a6c0e37b66d77a4
Allie <alliecaton3@gmail.com>
2021-07-23 20:26:23 -0400

    first commit. schema setup

---
+++ schemas/art.js
@@ -0,0 +1,35 @@
+export default {
+	name: 'art',
+	title: 'Art',
+	type: 'document',
+	fields: [
+		{
+			name: 'title',
+			type: 'string',
+		},
+		{
+			name: 'slug',
+			title: 'Slug',
+			type: 'slug',
+			options: {
+				source: 'title',
+				maxLength: 96,
+			},
+		},
+		{
+			name: 'body',
+			title: 'Body',
+			type: 'text',
+		},
+		{
+			name: 'images',
+			title: 'Images',
+			type: 'array',
+			of: [
+				{
+					type: 'image',
+				},
+			],
+		},
+	],
+}
